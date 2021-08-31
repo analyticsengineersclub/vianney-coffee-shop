9b0b1b80cf2e8a339a1c9e8f0aa6491a5e096ae1
Ubuntu <ubuntu@ip-172-31-17-149.us-east-2.compute.internal>
2021-08-31 02:40:18 +0000

    adding work from lecture 201

---
+++ models/customers.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='table'
+) }}
+with customer_orders as (
+    select
+        customer_id,
+        count(*) as number_of_orders,
+        min(created_at) as first_order_at,
+        sum(total) as total_order_value
+    from {{ source('coffee_shop', 'orders') }} as orders
+    group by 1
+)
+
+select
+    c.id AS customer_id,
+    c.name,
+    c.email,
+    co.first_order_at,
+    co.number_of_orders,
+    co.total_order_value
+
+from {{ source('coffee_shop', 'customers') }} as c
+left join customer_orders as co
+    on c.id = co.customer_id
---
+++ models/monthy_customers.sql
@@ -0,0 +1,8 @@
+select 
+    date_trunc((first_order_at, month) as date_month),
+
+    count (*) as new_customers
+
+from {{ref ('customers') }}
+
+group by 1
