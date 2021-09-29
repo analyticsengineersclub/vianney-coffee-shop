49a7466f89dec6021967008f6c27c1846ab205bc
Ubuntu <ubuntu@ip-172-31-17-149.us-east-2.compute.internal>
2021-09-29 20:53:21 +0000

    used if not loop and endif to remove last comma and move dat back to first row and group by 1

--- models/jinja_exercise_flavored.sql
+++ models/jinja_exercise_flavored.sql
@@ -1,13 +1,11 @@
 select
+ date_trunc(sold_at, month) as date_month,
 
   {% for product_category in ['coffee_beans','merch','brewing_supplies'] %} 
-  
   sum(case when product_category = '{{product_category}}' 
-    then total_price end) as {{product_category}}_amount,
-  
+    then total_price end) as {{product_category}}_amount
+  {% if not loop.last %},{% endif %}
   {%endfor%}
-
-  date_trunc(sold_at, month) as date_month
  
 from {{ ref('weekly_revenue_per_category') }}
-group by date_month
+group by 1
